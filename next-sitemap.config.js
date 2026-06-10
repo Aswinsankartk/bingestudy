dff9b23e470eca32a25096ca552156ffd1632ade
Aswinsankartk <164378656+Aswinsankartk@users.noreply.github.com>
2026-06-10 17:24:25 +0000

    Revert "Add sitemap"
    This reverts commit d5aebaa2fda6e1e6b2e05ea42feb8e0646a4c60e.

--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: "https://bingestudy.vercel.app", // Your website URL
-  generateRobotsTxt: true, // Generates a robots.txt file automatically
-  generateIndexSitemap: false, // Set to true if you have over 50,000 URLs
-  outDir: "public", // Outputs the files directly to your public folder
-};
