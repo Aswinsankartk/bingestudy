8389ba36407eef30068493766d3784223f714dab
Aswinsankartk <164378656+Aswinsankartk@users.noreply.github.com>
2026-07-05 10:26:20 +0530

    Revert "Add privacy policy page with basic UI changes"
    This reverts commit 687616c99b8b144dbf949afc64c148b3b13fe47d.

--- app/privacy/page.js
+++
@@ -1,9 +0,0 @@
-export default function PrivacyPage() {
-  return (
-    <iframe
-      src="/privacy-policy.html"
-      title="Privacy Policy"
-      className="w-full h-screen border-0"
-    />
-  );
-}
